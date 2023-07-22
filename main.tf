5f112fdc5e31037486aa756cee605192c56d5153
amlandutta95 <amlandutta204@gmail.com>
2023-07-22 21:46:46 +0530

    added tags

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~=3.66.0"
+      version = "~>3.66.0"
     }
   }
 }
@@ -16,13 +16,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "my-teff123"
   location = "East US"
+  tags = {
+    environment = "dev"
+  }
 
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }

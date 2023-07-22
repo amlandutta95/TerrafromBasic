e7c5e44d0d6a8b27644d20acf3c8fc8ac3b141c3
amlandutta95 <amlandutta204@gmail.com>
2023-07-22 19:16:07 +0530

    corrected code teff blocl

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_version = "~>1.4.6"
   required_providers {
-    azurerm {
-        source ="hashicorp/azurerm"
-        version=">~2.52.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">~2.52.0"
     }
   }
 }
 
 provider "azurerm" {
-    features{}
-    skip_provider_registration = true
+  features {}
+  skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "my-teff123"
-    location = "East US"
+  name     = "my-teff123"
+  location = "East US"
 
 }

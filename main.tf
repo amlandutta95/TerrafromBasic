b84fe1b640b23c8ff7e644ac46b211354d49930f
amlandutta95 <amlandutta204@gmail.com>
2023-07-22 19:54:52 +0530

    Added Storage Account to Code

--- main.tf
+++ main.tf
@@ -18,3 +18,11 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
